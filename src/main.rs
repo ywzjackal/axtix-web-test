dd61874fedd16b24cdc4b53ae1a35f3e81187f79
ywzjackal <ywzjackal@163.com>
2018-05-17 12:46:26 +0800

    panicked at 'index out of bounds: the len is 3 but the index is 3'
    Signed-off-by: ywzjackal <ywzjackal@163.com>

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,36 @@
 extern crate actix;
 extern crate actix_web;
 
-use actix::*;
 use actix_web::*;
-use std::cell::Cell;
 
-struct AppState {
-    counter: Cell<usize>,
+struct AppState;
+
+fn index<'a>(_req: HttpRequest<AppState>) -> &'a str {
+    "Hello"
 }
 
-fn index(req: HttpRequest<AppState>) -> String {
-    let count = req.state().counter.get() + 1; // <- get count
-    req.state().counter.set(count);            // <- store new count in state
-
-    format!("Request number: {}", count)       // <- response with count
-}
 // will fail
 fn main() {
     let sys = actix::System::new("guide");
     server::HttpServer::new(
-        || vec![
-            App::with_state(AppState { counter: Cell::new(0) })
-                .resource("/", |r| r.f(index))
-        ])
+        ||
+            {
+                let cors = actix_web::middleware::cors::Cors::build()
+                    .allowed_methods(vec!["GET", "POST", "OPTION", "DELETE", "PUT"])
+                    .supports_credentials()
+                    .max_age(3600)
+                    .finish();
+                vec![
+                    App::with_state(AppState)
+                        .middleware(actix_web::middleware::Logger::default())
+                        .middleware(actix_web::middleware::session::SessionStorage::new(actix_web::middleware::session::CookieSessionBackend::signed(&[1; 32]).secure(false)))
+                        .middleware(cors.clone())
+                        .resource("/", |r| r.f(index))
+                        .boxed()
+                ]
+            })
         .bind("127.0.0.1:8080").expect("Can not bind to 127.0.0.1:8080")
         .workers(8)
         .start();
     let _ = sys.run();
 }
-
-// will success
-//fn main() {
-//    let sys = actix::System::new("guide");
-//    server::HttpServer::new(
-//        || {
-//            App::with_state(AppState { counter: Cell::new(0) })
-//                .resource("/", |r| r.f(index))
-//        })
-//        .bind("127.0.0.1:8080").expect("Can not bind to 127.0.0.1:8080")
-//        .workers(8)
-//        .start();
-//    let _ = sys.run();
-//}
